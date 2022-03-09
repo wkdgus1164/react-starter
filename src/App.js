d0143a4a1f2ba6cd305da65dcdf8895d45b30e57
Janghyun Han <wkdgus1164@naver.com>
2022-03-09 16:48:11 +0900

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React WITH JENKINS!
         </a>
       </header>
     </div>
